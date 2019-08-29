91fb1a5b9d041652d243b706c90acbcd9c786d9c
Rubrob <32711205+Rubrob@users.noreply.github.com>
2019-08-29 18:39:01 +0300

    Update mailer.js

--- mailer.js
+++ mailer.js
@@ -1,1 +1,24 @@
 
+
+// Transparter Setup
+const transporter = nodemailer.createTransport(
+  {
+    pool: true,
+    maxConnections: 10,
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: process.env.CLIENT_SMPT_EMAIL, // set your SMTP email
+      pass: process.env.CLIENT_SMPT_PASSWORD // set your SMTP password
+    }
+  },
+  {
+    from: `E-Store <${process.env.CLIENT_SMPT_EMAIL}>`,
+  }
+)
+
+// Create transporter mailer function
+const send = message => transporter.sendMail({ ...message }, (err, info) => err ? console.log(err) : console.log(`Email sent: ` + info));
+
+module.export = send
